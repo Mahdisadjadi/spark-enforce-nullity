9aabac8338baf8ec530e8f8290a18cfb6fb82f06
mahdisadjadi <sadjadi.seyedmahdi@gmail.com>
2022-06-06 13:37:49 -0700

    add output to file

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ spark = SparkSession.builder.master("local").getOrCreate()
 # Schema to enforce
 schema = StructType([
     StructField("a", IntegerType(), False),
-    StructField("b", IntegerType(), True),
+    StructField("b", IntegerType(), False),
     ])
 
 # read the file as an rdd
@@ -19,4 +19,13 @@ rdd  = sc.textFile("./sample.csv")
 # enforce the schema
 rdd_with_schema = spark.read.schema(schema).csv(rdd)
 
-print (f"schema= {rdd_with_schema.schema[0]}")
+print (f"schema= {rdd_with_schema.schema}")
+
+"""
+output: 
+schema= StructType(
+    List(
+        StructField(a,IntegerType,false),
+        StructField(b,IntegerType,false))
+    )
+"""
