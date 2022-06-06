fb679ad3d5a99d43dbf40d5b57b286d2903f912d
mahdisadjadi <sadjadi.seyedmahdi@gmail.com>
2022-06-06 13:34:40 -0700

    add example

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # spark-enforce-nullity
 Enforce nullity of a column using schema
+
+To run:
+
+```
+spark-submit main.py
+```
---
+++ main.py
@@ -0,0 +1,22 @@
+from pyspark import SparkContext, SparkConf
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, IntegerType
+
+# Spark session and context
+conf = SparkConf().setAppName("app")
+sc = SparkContext(conf=conf)
+spark = SparkSession.builder.master("local").getOrCreate()
+
+# Schema to enforce
+schema = StructType([
+    StructField("a", IntegerType(), False),
+    StructField("b", IntegerType(), True),
+    ])
+
+# read the file as an rdd
+rdd  = sc.textFile("./sample.csv")
+
+# enforce the schema
+rdd_with_schema = spark.read.schema(schema).csv(rdd)
+
+print (f"schema= {rdd_with_schema.schema[0]}")
